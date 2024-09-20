2641177d97d72896e989b8c96ba7170a67ec78e2
LeandroMedvedev <medvedevleandro@outlook.com>
2024-09-20 11:17:39 -0300

    feat: setup initial project structure with folders and config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
 export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  // Adiciona Prettier como plugin
   {
-    ignores: ['node_modules/**', 'dist/**'], // Ignora arquivos e diretórios
-  },
-  {
-    files: ['**/*.js'], // Aplica as regras a todos os arquivos .js
-    languageOptions: {
-      ecmaVersion: 'latest', // Define a versão do ECMAScript que você está usando
-      sourceType: 'module', // Permite o uso de módulos ECMAScript
+    plugins: {
+      prettier,
     },
     rules: {
-      semi: ['error', 'always'], // Enforce the use of semicolons
-      quotes: ['error', 'single'], // Enforce the use of single quotes
-      'no-unused-vars': 'warn', // Warns about unused variables
-      indent: ['error', 2], // Enforce a 2-space indentation
+      'prettier/prettier': 'error', // Exibe erros para violações de formatação
     },
   },
+  // Desativa regras que podem conflitar com Prettier
+  configPrettier,
 ];
